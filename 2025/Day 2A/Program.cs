17c81c792bf48ca2ddfef3ee220207167c47bf81
suflors <iammagical.space@gmail.com>
2025-12-08 16:03:42 -0800

    Day 2A

---
+++ 2025/Day 2A/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Hello!");
+List<ulong> invalidIDs = [];
+string path = "input.txt";
+List<string> ranges = File.ReadAllLines(path).First().Split(',').ToList();
+
+foreach (string range in ranges)
+{
+	List<string> rangeList = range.Split('-').ToList();
+	ulong from = ulong.Parse(rangeList[0]);
+	ulong to = ulong.Parse(rangeList[1]);
+	for (ulong i = from; i <= to; i++)
+	{
+		if (i.ToString().Length % 2 == 1)
+		{
+			continue;
+		}
+		string id = i.ToString();
+		string firstHalf = id.Substring(0, id.Length / 2);
+		string secondHalf = id.Substring(id.Length / 2, id.Length / 2);
+		if (firstHalf == secondHalf)
+		{
+			invalidIDs.Add(i);
+		}
+	}
+}
+
+Console.WriteLine("Done!");
+ulong sum = 0;
+foreach (ulong id in invalidIDs)
+{
+	sum += id;
+}
+Console.WriteLine($"Sum of invalid IDs: {sum}");
