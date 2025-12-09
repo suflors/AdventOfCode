104a79687d3089925d61bacecf66ae65e6f0109c
suflors <iammagical.space@gmail.com>
2025-12-08 18:49:18 -0800

    2025 Day 4A

---
+++ 2025/Day 4A/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Hello!");
+string path = "input.txt";
+List<List<char>> departmentMap = File.ReadAllLines(path).Select(line => line.ToList()).ToList();
+int accessible = 0;
+
+List<int> dRow = [-1, -1, -1, 0, 0, 1, 1, 1];
+List<int> dCol = [-1, 0, 1, -1, 1, -1, 0, 1];
+for (int x = 0; x < departmentMap.Count; x++)
+{
+	for (int y = 0; y < departmentMap[x].Count; y++)
+	{
+		if (departmentMap[x][y] != '@')
+		{
+			continue;
+		}
+		int adjacent = 0;
+		for (int i = 0; i < 8; i++)
+		{
+			int newX = x + dCol[i];
+			int newY = y + dRow[i];
+			if (newX >= 0 && newX < departmentMap.Count && newY >= 0 && newY < departmentMap[x].Count)
+			{
+				char adjacentChar = departmentMap[newX][newY];
+				if (adjacentChar == '@')
+				{
+					adjacent++;
+				}
+			}
+		}
+		if (adjacent < 4)
+		{
+			accessible++;
+		}
+	}
+}
+
+Console.WriteLine("Done!");
+Console.WriteLine($"{accessible} rolls are accessible!");
