86135f85918e3de08aa6aaab1089da842701ef76
suflors <iammagical.space@gmail.com>
2025-12-09 16:25:48 -0800

    2025 Day 7

---
+++ 2025/Day 7A/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+Console.WriteLine("Hello!");
+string path = "input.txt";
+List<string> input = File.ReadAllLines(path).ToList();
+StringBuilder beam = new(input[0]);
+input.RemoveAt(0);
+int splits = 0;
+foreach (string line in input)
+{
+	for (int i = 0; i < beam.Length; i++)
+	{
+		if (line[i] == '^')
+		{
+			if (beam[i] == 'S')
+			{
+				splits++;
+				beam[i] = '.';
+				if (i - 1 >= 0)
+				{
+					beam[i - 1] = 'S';
+				}
+				if (i + 1 < beam.Length)
+				{
+					beam[i + 1] = 'S';
+				}
+			}
+		}
+	}
+}
+Console.WriteLine("Done!");
+Console.WriteLine($"Number of splits: {splits}");
---
+++ 2025/Day 7B/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.WriteLine("Hello!");
+string path = "input.txt";
+List<string> input = File.ReadAllLines(path).ToList();
+Dictionary<int, ulong> timelines = [];
+timelines.Add(input[0].IndexOf('S'), 1);
+
+foreach (string line in input.Skip(1))
+{
+	foreach (int key in timelines.Keys.ToList())
+	{
+		if (line[key] == '^')
+		{
+			ulong value = timelines[key];
+			if (key - 1 >= 0)
+			{
+				if (timelines.TryGetValue(key - 1, out ulong oldValue))
+				{
+					timelines[key - 1] = value + oldValue;
+				} else
+				{
+					timelines[key - 1] = value;
+				}
+			}
+			if (key + 1 < line.Length)
+			{
+				if (timelines.TryGetValue(key + 1, out ulong oldValue))
+				{
+					timelines[key + 1] = value + oldValue;
+				} else
+				{
+					timelines[key + 1] = value;
+				}
+			}
+			timelines.Remove(key);
+		}
+	}
+}
+
+ulong totalTimelines = 0;
+foreach (ulong value in timelines.Values)
+{
+	totalTimelines += value;
+}
+
+Console.WriteLine("Done!");
+Console.WriteLine($"Number of timelines: {totalTimelines}");
