5d63ae2fc42939438118cc4fa4d2a41d3826a6ef
suflors <iammagical.space@gmail.com>
2025-12-10 21:34:31 -0800

    2025 Day 9A

---
+++ 2025/Day 9A/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Hello!");
+string path = "input.txt";
+List<(long, long)> points = File
+	.ReadAllLines(path)
+	.Select(s => s
+		.Split(',')
+		.Select(s => long.Parse(s))
+		.ToList()
+	)
+	.Select(i => (i[0], i[1]))
+	.ToList();
+
+List<long> areas = [];
+for (int i = 0; i < points.Count; i++)
+{
+	for (int j = i + 1; j < points.Count; j++)
+	{
+		areas.Add((1 + Math.Abs(points[i].Item1 - points[j].Item1)) * (1 + Math.Abs(points[i].Item2 - points[j].Item2)));
+	}
+}
+long result = areas.Max();
+Console.WriteLine("Done!");
+Console.WriteLine($"Largest rectangle area: {result}");
